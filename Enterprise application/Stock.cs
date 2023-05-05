521075d25b030765f3c79757be77573fbe3a8b76
Imalsha Herath <86360205+imalsha-herath@users.noreply.github.com>
2023-05-05 11:32:57 +0530

    update stock

--- Enterprise application/Stock.cs
+++ Enterprise application/Stock.cs
@@ -34,7 +34,9 @@ namespace Enterprise_application
 
         private void button8_Click(object sender, EventArgs e)
         {
-
+          Employee obj = new Employee();
+            this.Hide();
+            obj.Show();
         }
 
         private void button7_Click(object sender, EventArgs e)
