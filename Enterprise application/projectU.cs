828a49ecbe72949484b46a72d11a92ed8c7419be
Deshaninayanamini <kldnayanamini@students.nsbm.ac.lk>
2023-05-05 12:56:30 +0530

    edit

--- Enterprise application/projectU.cs
+++ Enterprise application/projectU.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,10 +11,18 @@ using System.Windows.Forms;
 namespace Enterprise_application
 {
     public partial class projectU : Form
+
+       SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\Documents\login.mdf;Integrated Security=True;Connect Timeout=30");
+
     {
         public projectU()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
